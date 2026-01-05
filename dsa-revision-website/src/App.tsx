3e001690f3be2294152055d12a7351010eaae2a6
Tristan <tristanbudd@outlook.com>
2026-01-05 19:23:47 +0000

    Project Initialisation (+Favicon & SEO)

---
+++ dsa-revision-website/src/App.tsx
@@ -0,0 +1,11 @@
+import Message from "./SimpleMessage";
+
+function App() {
+    return (
+        <div className="App">
+            <Message />
+        </div>
+    );
+}
+
+export default App;
---
+++ dsa-revision-website/src/SimpleMessage.tsx
@@ -0,0 +1,5 @@
+function Message() {
+    return <h1>Welcome to UoP DSA Revision</h1>;
+}
+
+export default Message;
